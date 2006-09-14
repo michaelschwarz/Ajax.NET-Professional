0bdb5ba6cf028aa8f4ed9943e59f700f04e62f88
interactive_cp <SND\interactive_cp>
2006-09-14 13:39:10 +0000

    modified build batch files

---
+++ AjaxPro/build.bat
@@ -0,0 +1,7 @@
+set NET11="%WINDIR%\Microsoft.NET\Framework\v1.1.4322"
+set NET20="%WINDIR%\Microsoft.NET\Framework\v2.0.50727"
+set ARG=
+
+call build_1.1.bat
+call build_2.0.bat
+call build_json.bat
