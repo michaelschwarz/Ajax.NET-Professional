b66b314644bfae285d71d50ea405178c0d258f85
interactive_cp <SND\interactive_cp>
2006-09-29 13:13:14 +0000

    added new configuration to use DateTimes without the UTC information fixed renderDateTimeAsString serialization

--- AjaxPro/Utilities/Constant.cs
+++ AjaxPro/Utilities/Constant.cs
@@ -52,6 +52,6 @@ namespace AjaxPro
         /// <summary>
 		/// The assembly version.
 		/// </summary>
-		public const string AssemblyVersion = "6.9.29.1";
+		public const string AssemblyVersion = "6.9.29.2";
 	}
 }
